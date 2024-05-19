9f9444c8bb3434fca69128851234b21b586d848b
sarvesh.kadam <kadamsarvesh1905@gmail.com>
2024-05-19 15:28:39 +0530

    remove imp files

--- server/build/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserErrors = void 0;
-var UserErrors;
-(function (UserErrors) {
-    UserErrors["NO_USER_FOUND"] = "no-user-found";
-    UserErrors["WRONG_CREDENTIALS"] = "wrong-credentials";
-    UserErrors["USER_ALREADY_EXISTS"] = "user-already-exist";
-})(UserErrors || (exports.UserErrors = UserErrors = {}));
